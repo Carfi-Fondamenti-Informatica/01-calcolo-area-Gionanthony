e24192ef76ad86dd02027d2b7dcf48fde52e633e
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-11-11 11:27:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@ int main(){
     
 float a, b, c;
 cin >> a >> b >> c;
-
-    cout << (a*b)/2 << endl;
-    cout <<  a*a << endl;
-    cout <<  a*b << endl;
-    cout << ((a+b)*c)/2 << endl;
+cout << (a*b)/2 << endl;
+cout <<  a*a << endl;
+cout <<  a*b << endl;
+cout << ((a+b)*c)/2 << endl;
 }
