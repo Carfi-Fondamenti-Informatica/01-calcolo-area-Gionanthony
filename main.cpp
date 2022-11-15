011a9d1be59e8a737a3ddb5f648f450accf43509
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-11-15 16:58:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-    
+int main() {
 float a, b, c;
 cin >> a >> b >> c;
 cout << (a*b)/2 << endl;
 cout <<  a*a << endl;
 cout <<  a*b << endl;
 cout << ((a+b)*c)/2 << endl;
-    
-   return 0;
+return 0;
 }
